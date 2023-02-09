1b7bb51ab8df784a7e942b30cd1af0ea09acac35
patient-fyd <patient.fyd@gmail.com>
2023-02-10 02:24:45 +0800

    数据类型

---
+++ day01/03.py
@@ -0,0 +1,27 @@
+# 基础数据类型：int float bool
+# 容器类型：str tuple list ser dict
+
+# 整型
+int1 = 12
+print(type(int1))
+# 浮点型
+float1 = 12.1
+print(type(float1))
+# 布尔型
+bool1 = True
+print(type(bool1))
+# 字符串型
+str1 = 'hello python'
+print(type(str1))
+# 元组
+tuple1 = (1, 2, 3, 4)
+print(type(tuple1))
+# 列表
+list1 = [1, 2, 3, 4]
+print(type(list1))
+# 集合
+set1 = {1, 2, 3, 4}
+print(type(set1))
+# 字典
+dict1 = {'name': 'ZhangSan', 'age': '18'}
+print(type(dict1))
