82ec9015e2f39015bfafba5fc8a864e861a9474b
patient-fyd <patient.fyd@gmail.com>
2023-03-24 23:01:49 +0800

    Update 01列表的定义.py

--- day05/01列表的定义.py
+++ day05/01列表的定义.py
@@ -3,3 +3,8 @@ name_list = ['Rose', 'Jack', 'Tom']
 # 直接使用print打印列表结构
 print(name_list)
 print(type(name_list))
+
+# 注意：在python中 列表可以储存不同数据类型的数据
+# 但是为了方便使用和后期迭代，数组中一般储存同种数据类型数据
+list1 = ['name', 12, False, [1, 2, 3]]
+print(list1)
