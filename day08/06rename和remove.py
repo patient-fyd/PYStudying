bc1608ed1c7a4b96ff80a7c0f4f704d0315cae98
patient-fyd <patient.fyd@gmail.com>
2023-04-06 22:47:39 +0800

    Update 06rename和remove.py

--- day08/06rename和remove.py
+++ day08/06rename和remove.py
@@ -12,4 +12,10 @@ import os
 # 格式：os.remove(路径)
 # 删除文件不会进入回收站，直接删除，慎用。也没有提示
 os.remove("test[备份].txt")
-
+# 可以删除指定路径下的文件
+# 如果被删除的路径不存在,则报错  (路径可以使用绝对路径,和相对路径)
+# os.remove('文件/a.txt')
+# os.remove('python[备份].txt')
+# PermissionError: [Errno 1] Operation not permitted: '文件'
+# 使用remove不能删除文件夹
+os.remove('文件')
